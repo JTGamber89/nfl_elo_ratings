16913d615adbfafa4ca9c86fd19b7ed8341ebf44
LAPTOP-FGD34NBC\joseg <jgamber89@gmail.com>
2021-07-25 16:46:24 -0400

    Corrected two errors in team re-naming and removed the glimpse() command in global.R

--- global.R
+++ global.R
@@ -1,25 +1,25 @@
-#
-# Application:  NFL Team and QB Performance in the Super Bowl Era
-# Author:       Joseph T. Gamber
-# Course:       CMPINF 2130: The Art of Data Visualization
-# Submitted for Final Project Consideration
-####
+##
+## Application:  NFL Team and QB Performance in the Super Bowl Era
+## Author:       Joseph T. Gamber
+## Course:       CMPINF 2130: The Art of Data Visualization
+## Submitted for Final Project Consideration
+######
 
-# Load necessary R libraries
+## Load necessary R libraries
 library(tidyverse)
 library(teamcolors)
 
-# Load in data set from Github
+## Load in data set from Github
 nfl_elo <- readr::read_csv('https://raw.githubusercontent.com/JTG89/nfl_elo_data/main/nfl_elo_SB_era.csv', col_names = TRUE)
 
-# Re-Define the team names in the data set as the full team name rather than the abbreviation
+## Re-Define the team names in the data set as the full team name rather than the abbreviation
 nfl_elo$team1[nfl_elo$team1 == "ARI"] <- "Arizona Cardinals"
 nfl_elo$team1[nfl_elo$team1 == "ATL"] <- "Atlanta Falcons"
 nfl_elo$team1[nfl_elo$team1 == "BAL"] <- "Baltimore Ravens"
 nfl_elo$team1[nfl_elo$team1 == "BUF"] <- "Buffalo Bills"
 nfl_elo$team1[nfl_elo$team1 == "CAR"] <- "Carolina Panthers"
 nfl_elo$team1[nfl_elo$team1 == "CHI"] <- "Chicago Bears"
-nfl_elo$team1[nfl_elo$team1 == "CIN"] <- "Cincinatti Bengals"
+nfl_elo$team1[nfl_elo$team1 == "CIN"] <- "Cincinnati Bengals"
 nfl_elo$team1[nfl_elo$team1 == "CLE"] <- "Cleveland Browns"
 nfl_elo$team1[nfl_elo$team1 == "DAL"] <- "Dallas Cowboys"
 nfl_elo$team1[nfl_elo$team1 == "DEN"] <- "Denver Broncos"
@@ -37,14 +37,49 @@ nfl_elo$team1[nfl_elo$team1 == "NE"] <- "New England Patriots"
 nfl_elo$team1[nfl_elo$team1 == "NO"] <- "New Orleans Saints"
 nfl_elo$team1[nfl_elo$team1 == "NYG"] <- "New York Giants"
 nfl_elo$team1[nfl_elo$team1 == "NYJ"] <- "New York Jets"
-nfl_elo$team1[nfl_elo$team1 == "OAK"] <- "Las Vegas Raiders"
-nfl_elo$team1[nfl_elo$team1 == "PHI"] <- "Philidelphia Eagles"
+nfl_elo$team1[nfl_elo$team1 == "OAK"] <- "Oakland Raiders"
+nfl_elo$team1[nfl_elo$team1 == "PHI"] <- "Philadelphia Eagles"
 nfl_elo$team1[nfl_elo$team1 == "PIT"] <- "Pittsburgh Steelers"
 nfl_elo$team1[nfl_elo$team1 == "SEA"] <- "Seattle Seahawks"
 nfl_elo$team1[nfl_elo$team1 == "SF"] <- "San Francisco 49ers"
 nfl_elo$team1[nfl_elo$team1 == "TB"] <- "Tampa Bay Buccaneers"
 nfl_elo$team1[nfl_elo$team1 == "TEN"] <- "Tennessee Titans"
-nfl_elo$team1[nfl_elo$team1 == "WSH"] <- "Washington Football Team"
+nfl_elo$team1[nfl_elo$team1 == "WSH"] <- "Washington Redskins"
 
+nfl_elo$team2[nfl_elo$team2 == "ARI"] <- "Arizona Cardinals"
+nfl_elo$team2[nfl_elo$team2 == "ATL"] <- "Atlanta Falcons"
+nfl_elo$team2[nfl_elo$team2 == "BAL"] <- "Baltimore Ravens"
+nfl_elo$team2[nfl_elo$team2 == "BUF"] <- "Buffalo Bills"
+nfl_elo$team2[nfl_elo$team2 == "CAR"] <- "Carolina Panthers"
+nfl_elo$team2[nfl_elo$team2 == "CHI"] <- "Chicago Bears"
+nfl_elo$team2[nfl_elo$team2 == "CIN"] <- "Cincinnati Bengals"
+nfl_elo$team2[nfl_elo$team2 == "CLE"] <- "Cleveland Browns"
+nfl_elo$team2[nfl_elo$team2 == "DAL"] <- "Dallas Cowboys"
+nfl_elo$team2[nfl_elo$team2 == "DEN"] <- "Denver Broncos"
+nfl_elo$team2[nfl_elo$team2 == "DET"] <- "Detroit Lions"
+nfl_elo$team2[nfl_elo$team2 == "GB"] <- "Green Bay Packers"
+nfl_elo$team2[nfl_elo$team2 == "HOU"] <- "Houston Texans"
+nfl_elo$team2[nfl_elo$team2 == "IND"] <- "Indianapolis Colts"
+nfl_elo$team2[nfl_elo$team2 == "JAX"] <- "Jacksonville Jaguars"
+nfl_elo$team2[nfl_elo$team2 == "KC"] <- "Kansas City Chiefs"
+nfl_elo$team2[nfl_elo$team2 == "LAC"] <- "Los Angeles Chargers"
+nfl_elo$team2[nfl_elo$team2 == "LAR"] <- "Los Angeles Rams"
+nfl_elo$team2[nfl_elo$team2 == "MIA"] <- "Miami Dolphins"
+nfl_elo$team2[nfl_elo$team2 == "MIN"] <- "Minnesota Vikings"
+nfl_elo$team2[nfl_elo$team2 == "NE"] <- "New England Patriots"
+nfl_elo$team2[nfl_elo$team2 == "NO"] <- "New Orleans Saints"
+nfl_elo$team2[nfl_elo$team2 == "NYG"] <- "New York Giants"
+nfl_elo$team2[nfl_elo$team2 == "NYJ"] <- "New York Jets"
+nfl_elo$team2[nfl_elo$team2 == "OAK"] <- "Oakland Raiders"
+nfl_elo$team2[nfl_elo$team2 == "PHI"] <- "Philadelphia Eagles"
+nfl_elo$team2[nfl_elo$team2 == "PIT"] <- "Pittsburgh Steelers"
+nfl_elo$team2[nfl_elo$team2 == "SEA"] <- "Seattle Seahawks"
+nfl_elo$team2[nfl_elo$team2 == "SF"] <- "San Francisco 49ers"
+nfl_elo$team2[nfl_elo$team2 == "TB"] <- "Tampa Bay Buccaneers"
+nfl_elo$team2[nfl_elo$team2 == "TEN"] <- "Tennessee Titans"
+nfl_elo$team2[nfl_elo$team2 == "WSH"] <- "Washington Redskins"
 
-nfl_elo %>% glimpse()
+#nfl_colors <- teamcolors %>%
+#  filter(league == "nfl") %>% 
+#  mutate(name = ifelse(name == "Washington Redskins", "Washington Football Team", name)) %>% 
+#  mutate(name = ifelse(name == "Oakland Raiders", "Las Vegas Raiders", name))
--- ui.R
+++ ui.R
@@ -1,9 +1,9 @@
-#
-# Application:  NFL Team and QB Performance in the Super Bowl Era
-# Author:       Joseph T. Gamber
-# Course:       CMPINF 2130: The Art of Data Visualization
-# Submitted for Final Project Consideration
-####
+##
+## Application:  NFL Team and QB Performance in the Super Bowl Era
+## Author:       Joseph T. Gamber
+## Course:       CMPINF 2130: The Art of Data Visualization
+## Submitted for Final Project Consideration
+######
 
 library(shiny)
 
