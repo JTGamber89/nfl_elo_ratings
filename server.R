b0c8e17bbf3a3c654dcad7662f1ea1d2b3fe21f0
LAPTOP-FGD34NBC\joseg <jgamber89@gmail.com>
2021-07-27 21:15:40 -0400

    Cleared previous crap to pave the way for the glorious new plan!

--- server.R
+++ server.R
@@ -8,20 +8,10 @@
 library(shiny)
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
+  
+  
 
-    output$team_weekly_trace <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        team_season <- nfl_elo %>% filter(season == selected_season,
-                                   team1 == selected_team | team2 == selected_team) %>%
-          rowid_to_column("Week")
-                                   
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-    })
-
+    
+  
 })
