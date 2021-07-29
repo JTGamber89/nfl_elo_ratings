dcade0b96b0e3afe16cc67d68321a460dd4f9726
LAPTOP-FGD34NBC\joseg <jgamber89@gmail.com>
2021-07-28 20:32:03 -0400

    Added first reactive action to limit teams based on whether they existe in a given year

--- server.R
+++ server.R
@@ -10,7 +10,21 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
   
-  
+  observe({
+    selected_season <- input$panel1_season
+    
+    active_teams <- nfl_elo %>%
+      filter(season == selected_season) %>%
+      select(team1) %>%
+      dplyr::distinct(team1)
+    
+    active_teams <- active_teams$team1 %>% sort()
+    
+    updateSelectInput(session,
+                      "panel1_team",
+                      choices = active_teams,
+                      selected = active_teams[1])
+  })
 
     
   
