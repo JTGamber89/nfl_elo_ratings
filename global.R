e0cae28b24c77421a14b0e985941741b7f382c22
LAPTOP-FGD34NBC\joseg <jgamber89@gmail.com>
2021-07-31 15:23:09 -0400

    Added logic to pivot data longer and include home v away

--- global.R
+++ global.R
@@ -13,6 +13,9 @@ library(plotly)
 ## Load in data set from Github
 nfl_elo <- readr::read_csv('https://raw.githubusercontent.com/JTG89/nfl_elo_ratings/main/nfl_elo_SB_era.csv', col_names = TRUE)
 
+
+###### Initial Data Pre-Processing ######
+
 ## Format date column using the lubridate package and store as separate columns
 nfl_elo <- nfl_elo %>% mutate(year = lubridate::year(lubridate::mdy(date)), .after = season) %>% 
   mutate(month = lubridate::month(lubridate::mdy(date)), .after = year) %>%
@@ -43,78 +46,82 @@ nfl_elo <- nfl_elo %>%
 # Reorder columns within data set
 nfl_elo <- nfl_elo[,c(1,2,3,4,5,6,7,36,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35)]
 
+## Re-order dataframe into longer format for home & away
+nfl_elo_home <- nfl_elo %>%  select(!contains('2')) %>% 
+  mutate(home_away = 'Home', .after = 'team1') %>% 
+  rename(
+    team = team1,
+    elo_pre = elo1_pre,
+    elo_prob = elo_prob1,
+    elo_post = elo1_post,
+    qbelo_pre = qbelo1_pre,
+    qb = qb1,
+    qb_value_pre = qb1_value_pre,
+    qb_adj = qb1_adj,
+    qbelo_prob = qbelo_prob1,
+    qb_game_value = qb1_game_value,
+    qb_value_post = qb1_value_post,
+    qbelo_post = qbelo1_post,
+    score = score1
+  )
+
+nfl_elo_away <- nfl_elo %>%  select(!contains('1')) %>% 
+  mutate(home_away = 'Away', .after = 'team2') %>% 
+  rename(
+    team = team2,
+    elo_pre = elo2_pre,
+    elo_prob = elo_prob2,
+    elo_post = elo2_post,
+    qbelo_pre = qbelo2_pre,
+    qb = qb2,
+    qb_value_pre = qb2_value_pre,
+    qb_adj = qb2_adj,
+    qbelo_prob = qbelo_prob2,
+    qb_game_value = qb2_game_value,
+    qb_value_post = qb2_value_post,
+    qbelo_post = qbelo2_post,
+    score = score2
+  )
+
+nfl_elo <- rbind(nfl_elo_home, nfl_elo_away) %>% 
+  arrange(season, year, month, day)
 
 ## Re-Define the team names in the data set as the full team name rather than the abbreviation
-nfl_elo$team1[nfl_elo$team1 == "ARI"] <- "Arizona Cardinals"
-nfl_elo$team1[nfl_elo$team1 == "ATL"] <- "Atlanta Falcons"
-nfl_elo$team1[nfl_elo$team1 == "BAL"] <- "Baltimore Ravens"
-nfl_elo$team1[nfl_elo$team1 == "BUF"] <- "Buffalo Bills"
-nfl_elo$team1[nfl_elo$team1 == "CAR"] <- "Carolina Panthers"
-nfl_elo$team1[nfl_elo$team1 == "CHI"] <- "Chicago Bears"
-nfl_elo$team1[nfl_elo$team1 == "CIN"] <- "Cincinnati Bengals"
-nfl_elo$team1[nfl_elo$team1 == "CLE"] <- "Cleveland Browns"
-nfl_elo$team1[nfl_elo$team1 == "DAL"] <- "Dallas Cowboys"
-nfl_elo$team1[nfl_elo$team1 == "DEN"] <- "Denver Broncos"
-nfl_elo$team1[nfl_elo$team1 == "DET"] <- "Detroit Lions"
-nfl_elo$team1[nfl_elo$team1 == "GB"] <- "Green Bay Packers"
-nfl_elo$team1[nfl_elo$team1 == "HOU"] <- "Houston Texans"
-nfl_elo$team1[nfl_elo$team1 == "IND"] <- "Indianapolis Colts"
-nfl_elo$team1[nfl_elo$team1 == "JAX"] <- "Jacksonville Jaguars"
-nfl_elo$team1[nfl_elo$team1 == "KC"] <- "Kansas City Chiefs"
-nfl_elo$team1[nfl_elo$team1 == "LAC"] <- "Los Angeles Chargers"
-nfl_elo$team1[nfl_elo$team1 == "LAR"] <- "Los Angeles Rams"
-nfl_elo$team1[nfl_elo$team1 == "MIA"] <- "Miami Dolphins"
-nfl_elo$team1[nfl_elo$team1 == "MIN"] <- "Minnesota Vikings"
-nfl_elo$team1[nfl_elo$team1 == "NE"] <- "New England Patriots"
-nfl_elo$team1[nfl_elo$team1 == "NO"] <- "New Orleans Saints"
-nfl_elo$team1[nfl_elo$team1 == "NYG"] <- "New York Giants"
-nfl_elo$team1[nfl_elo$team1 == "NYJ"] <- "New York Jets"
-nfl_elo$team1[nfl_elo$team1 == "OAK"] <- "Oakland Raiders"
-nfl_elo$team1[nfl_elo$team1 == "PHI"] <- "Philadelphia Eagles"
-nfl_elo$team1[nfl_elo$team1 == "PIT"] <- "Pittsburgh Steelers"
-nfl_elo$team1[nfl_elo$team1 == "SEA"] <- "Seattle Seahawks"
-nfl_elo$team1[nfl_elo$team1 == "SF"] <- "San Francisco 49ers"
-nfl_elo$team1[nfl_elo$team1 == "TB"] <- "Tampa Bay Buccaneers"
-nfl_elo$team1[nfl_elo$team1 == "TEN"] <- "Tennessee Titans"
-nfl_elo$team1[nfl_elo$team1 == "WSH"] <- "Washington Redskins"
+nfl_elo$team[nfl_elo$team == "ARI"] <- "Arizona Cardinals"
+nfl_elo$team[nfl_elo$team == "ATL"] <- "Atlanta Falcons"
+nfl_elo$team[nfl_elo$team == "BAL"] <- "Baltimore Ravens"
+nfl_elo$team[nfl_elo$team == "BUF"] <- "Buffalo Bills"
+nfl_elo$team[nfl_elo$team == "CAR"] <- "Carolina Panthers"
+nfl_elo$team[nfl_elo$team == "CHI"] <- "Chicago Bears"
+nfl_elo$team[nfl_elo$team == "CIN"] <- "Cincinnati Bengals"
+nfl_elo$team[nfl_elo$team == "CLE"] <- "Cleveland Browns"
+nfl_elo$team[nfl_elo$team == "DAL"] <- "Dallas Cowboys"
+nfl_elo$team[nfl_elo$team == "DEN"] <- "Denver Broncos"
+nfl_elo$team[nfl_elo$team == "DET"] <- "Detroit Lions"
+nfl_elo$team[nfl_elo$team == "GB"] <- "Green Bay Packers"
+nfl_elo$team[nfl_elo$team == "HOU"] <- "Houston Texans"
+nfl_elo$team[nfl_elo$team == "IND"] <- "Indianapolis Colts"
+nfl_elo$team[nfl_elo$team == "JAX"] <- "Jacksonville Jaguars"
+nfl_elo$team[nfl_elo$team == "KC"] <- "Kansas City Chiefs"
+nfl_elo$team[nfl_elo$team == "LAC"] <- "Los Angeles Chargers"
+nfl_elo$team[nfl_elo$team == "LAR"] <- "Los Angeles Rams"
+nfl_elo$team[nfl_elo$team == "MIA"] <- "Miami Dolphins"
+nfl_elo$team[nfl_elo$team == "MIN"] <- "Minnesota Vikings"
+nfl_elo$team[nfl_elo$team == "NE"] <- "New England Patriots"
+nfl_elo$team[nfl_elo$team == "NO"] <- "New Orleans Saints"
+nfl_elo$team[nfl_elo$team == "NYG"] <- "New York Giants"
+nfl_elo$team[nfl_elo$team == "NYJ"] <- "New York Jets"
+nfl_elo$team[nfl_elo$team == "OAK"] <- "Oakland Raiders"
+nfl_elo$team[nfl_elo$team == "PHI"] <- "Philadelphia Eagles"
+nfl_elo$team[nfl_elo$team == "PIT"] <- "Pittsburgh Steelers"
+nfl_elo$team[nfl_elo$team == "SEA"] <- "Seattle Seahawks"
+nfl_elo$team[nfl_elo$team == "SF"] <- "San Francisco 49ers"
+nfl_elo$team[nfl_elo$team == "TB"] <- "Tampa Bay Buccaneers"
+nfl_elo$team[nfl_elo$team == "TEN"] <- "Tennessee Titans"
+nfl_elo$team[nfl_elo$team == "WSH"] <- "Washington Redskins"
 
-nfl_elo$team2[nfl_elo$team2 == "ARI"] <- "Arizona Cardinals"
-nfl_elo$team2[nfl_elo$team2 == "ATL"] <- "Atlanta Falcons"
-nfl_elo$team2[nfl_elo$team2 == "BAL"] <- "Baltimore Ravens"
-nfl_elo$team2[nfl_elo$team2 == "BUF"] <- "Buffalo Bills"
-nfl_elo$team2[nfl_elo$team2 == "CAR"] <- "Carolina Panthers"
-nfl_elo$team2[nfl_elo$team2 == "CHI"] <- "Chicago Bears"
-nfl_elo$team2[nfl_elo$team2 == "CIN"] <- "Cincinnati Bengals"
-nfl_elo$team2[nfl_elo$team2 == "CLE"] <- "Cleveland Browns"
-nfl_elo$team2[nfl_elo$team2 == "DAL"] <- "Dallas Cowboys"
-nfl_elo$team2[nfl_elo$team2 == "DEN"] <- "Denver Broncos"
-nfl_elo$team2[nfl_elo$team2 == "DET"] <- "Detroit Lions"
-nfl_elo$team2[nfl_elo$team2 == "GB"] <- "Green Bay Packers"
-nfl_elo$team2[nfl_elo$team2 == "HOU"] <- "Houston Texans"
-nfl_elo$team2[nfl_elo$team2 == "IND"] <- "Indianapolis Colts"
-nfl_elo$team2[nfl_elo$team2 == "JAX"] <- "Jacksonville Jaguars"
-nfl_elo$team2[nfl_elo$team2 == "KC"] <- "Kansas City Chiefs"
-nfl_elo$team2[nfl_elo$team2 == "LAC"] <- "Los Angeles Chargers"
-nfl_elo$team2[nfl_elo$team2 == "LAR"] <- "Los Angeles Rams"
-nfl_elo$team2[nfl_elo$team2 == "MIA"] <- "Miami Dolphins"
-nfl_elo$team2[nfl_elo$team2 == "MIN"] <- "Minnesota Vikings"
-nfl_elo$team2[nfl_elo$team2 == "NE"] <- "New England Patriots"
-nfl_elo$team2[nfl_elo$team2 == "NO"] <- "New Orleans Saints"
-nfl_elo$team2[nfl_elo$team2 == "NYG"] <- "New York Giants"
-nfl_elo$team2[nfl_elo$team2 == "NYJ"] <- "New York Jets"
-nfl_elo$team2[nfl_elo$team2 == "OAK"] <- "Oakland Raiders"
-nfl_elo$team2[nfl_elo$team2 == "PHI"] <- "Philadelphia Eagles"
-nfl_elo$team2[nfl_elo$team2 == "PIT"] <- "Pittsburgh Steelers"
-nfl_elo$team2[nfl_elo$team2 == "SEA"] <- "Seattle Seahawks"
-nfl_elo$team2[nfl_elo$team2 == "SF"] <- "San Francisco 49ers"
-nfl_elo$team2[nfl_elo$team2 == "TB"] <- "Tampa Bay Buccaneers"
-nfl_elo$team2[nfl_elo$team2 == "TEN"] <- "Tennessee Titans"
-nfl_elo$team2[nfl_elo$team2 == "WSH"] <- "Washington Redskins"
 
-#nfl_colors <- teamcolors %>%
-#  filter(league == "nfl") %>% 
-#  mutate(name = ifelse(name == "Washington Redskins", "Washington Football Team", name)) %>% 
-#  mutate(name = ifelse(name == "Oakland Raiders", "Las Vegas Raiders", name))
+###### Selection Lists of Unique Values ######
 
 nfl_seasons <- nfl_elo$season %>% unique()
 
